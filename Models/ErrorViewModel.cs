331a0d16511cd7c4671e750a241022627eb695e3
Broghan Kendra Enoch <131599287+ST10375560@users.noreply.github.com>
2024-10-08 14:28:06 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ICE_TASK_2._1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
